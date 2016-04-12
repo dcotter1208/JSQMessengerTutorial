20d04d37ae463efbcc21d78fe8c56506b16be9ec
Donovan Cotter <donovan.cotter@yahoo.com>
2016-04-12 12:23:16 -0400

    first commit

---
+++ Pods/Target Support Files/Pods-UdemyJSQMessenger/Pods-UdemyJSQMessenger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_UdemyJSQMessengerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UdemyJSQMessengerVersionString[];
+
---
+++ UdemyJSQMessenger/LoginVC.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  UdemyJSQMessenger
+//
+//  Created by Donovan Cotter on 4/12/16.
+//  Copyright © 2016 DonovanCotter. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var enterChatButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        //We put it in viewDidAppear because it runs when our view is loaded.
+        
+        UIView.animateWithDuration(1) { () -> Void in
+            
+            let logoImageYMoveTo = self.view.center.y - (self.usernameTextField.center.y - self.logoImageView.center.y)
+            let enterChatButtonYMoveTo = self.view.center.y - (self.usernameTextField.center.y - self.enterChatButton.center.y)
+            
+            //This will center our textfield and button with the center y axis.
+            self.usernameTextField.center.y = self.view.center.y
+            self.enterChatButton.center.y = enterChatButtonYMoveTo
+            self.logoImageView.center.y = logoImageYMoveTo
+            
+            self.usernameTextField.alpha = 1
+            self.enterChatButton.alpha = 1
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func enterChatButtonPressed(sender: AnyObject) {
+        
+        if self.usernameTextField.text != "" {
+            self.performSegueWithIdentifier("startChatting", sender: self)
+        }
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+
+}
+
